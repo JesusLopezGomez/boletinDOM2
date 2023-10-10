7f8ddbb82050aae0be88b72a211eb5fa780d634b
JesuLopezGomez <jelopez.gom@gmail.com>
2023-10-10 11:38:33 +0200

    terminado

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+//Ejercicio 1: Eventos Básicos
+//1.1. Crea una página HTML con un botón. Agrega un escuchador de eventos para que cuando se haga clic en el botón, se muestre un mensaje en la consola del navegador.
+let boton = document.getElementById("ejercicio1");
+boton.addEventListener(`click`,() => {
+    console.log("Hola que tal");
+})
+//1.2. Agrega otro elemento HTML (por ejemplo, un enlace) en la página y agrega un escuchador de eventos para que cuando se pase el mouse sobre el elemento, el color de fondo cambie.
+let enlace = document.getElementById("ejercicio1.2");
+enlace.addEventListener(`mousemove`,() => {
+    document.body.style.background = "red";
+})
+//1.3. Agrega un escuchador de eventos al elemento document.body para que cuando se mueva el ratón en cualquier punto de la ventana del navegador,
+// muestre en algún elemento (puedes agregar un <div> o <p> al HTML) la posición del puntero respecto al navegador y respecto a la página.
+document.body.addEventListener(`mousemove`,(event) => {
+    document.body.appendChild(document.createElement("div").appendChild(document.createTextNode("posX " + event.clientX,"posY " +event.clientY)));
+})
+
+//Ejercicio 2: Escuchar eventos de teclado
+//2.1 Agrega un escuchador de eventos al elemento document.body para que cuando se presione cualquier tecla, 
+//muestre un mensaje de alerta con el nombre de la tecla pulsada y su código.
+
+document.body.addEventListener(`keypress`,(event) => {
+    alert(event.key + " " + event.keyCode);
+
+})
 //Ejercicio 3: Captura y Propagación
 //3.1. Crea una lista desordenada (<ul>) con varios elementos de lista (<li>) dentro. 
 //Agrega un escuchador de eventos al elemento <ul> para que cuando se haga clic en cualquier elemento de la lista,
